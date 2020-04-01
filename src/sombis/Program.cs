5b1889e1f522ee59ea1f072298261331a2bff1f3
Thomas Harold <tgharold@gmail.com>
2020-04-01 07:41:08 -0400

    create project template files

---
+++ src/sombis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sombis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
