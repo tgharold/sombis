a7c201a85a9fee25617bfe8833c69e6fa1decf24
Thomas Harold <tgharold@gmail.com>
2020-04-02 10:19:13 -0400

    Setting up console app to analyze MBIs
    Will generate a lot of MBIs and analyze the
    frequency of characters in each position.

---
+++ analysis/sombis.DistibutionCheck/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sombis.DistibutionCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
