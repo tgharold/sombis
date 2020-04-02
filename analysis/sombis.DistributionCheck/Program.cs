c619aef9a1ed97add3aed3acc0cef3b8660427e2
Thomas Harold <tgharold@gmail.com>
2020-04-02 10:38:35 -0400

    Run 1 million iterations and record results

--- analysis/sombis.DistributionCheck/Program.cs
+++ analysis/sombis.DistributionCheck/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using sombis.Core;
 
 namespace sombis.DistributionCheck
@@ -8,8 +9,36 @@ namespace sombis.DistributionCheck
         static void Main(string[] args)
         {
             var generator = new Generator();
+            const int iterations = 1000000;
+            var results = new Dictionary<int, Dictionary<char, int>>();
             
-            Console.WriteLine("Hello World!");
+            for (var i = 1; i <= iterations; i++)
+            {
+                var mbi = generator.CreateRandomMbi();
+                var chars = mbi.ToCharArray();
+
+                for (var ci = 0; ci < chars.Length; ci++)
+                {
+                    UpdateResults(results, ci, chars[ci]);
+                }
+            }
+        }
+
+        private static void UpdateResults(
+            Dictionary<int, Dictionary<char, int>> results, 
+            int ci, 
+            char c
+            )
+        {
+            if (!results.ContainsKey(ci))
+                results.Add(ci, new Dictionary<char, int>());
+
+            var inner = results[ci];
+            
+            if (!inner.ContainsKey(c))
+                inner.Add(c, 0);
+
+            inner[c] = inner[c] + 1;
         }
     }
 }
